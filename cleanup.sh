afa2e287827ad818a82a3b9f46d2161204039218
wtmpx <ahmad.zayed@outlook.com>
2020-02-12 16:07:22 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,8 @@
 
 systemctl --user stop SFSCextractor@$USER.service
 systemctl --user disable SFSCextractor@$USER.service
-systemctl --user daemon-reload
 
 rm ~/.SFSCextractorrc
 sudo rm /usr/bin/SFSCextractor.sh /etc/systemd/user/SFSCextractor@.service 
+
+systemctl --user daemon-reload
