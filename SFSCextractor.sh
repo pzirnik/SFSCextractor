9acb7dde2836356dac94a04e6d00f8cc8f2c80e8
Paul Zirnik <paul.zirnik@suse.com>
2020-02-18 16:54:16 +0100

    escape filenames

--- SFSCextractor.sh
+++ SFSCextractor.sh
@@ -29,7 +29,7 @@ log() {
 # in this case tar does report success, which is wrong
 # as nothing will be done 
 is_tar() {
-	FILELIST=$(/usr/bin/tar --list -f ${1})
+	FILELIST=$(/usr/bin/tar --list -f "${1}")
 	if [ -z "${FILELIST}" -o $? -ne 0 ] ; then
 		return -1;
 	fi
@@ -45,7 +45,7 @@ handle_file() {
 	# create directory
 	mkdir -p ${CASES_FOLDER}/${CASENO} || log 1 "Could not create directory ${CASES_FOLDER}/${CASENO}"
 	# compute real filename
-	REALNAME=$(echo ${FILENAME} | sed -ne "s/^SFSC[0-9]\{8,\}_\(.*\)$/\1/p")
+	REALNAME=$(echo "${FILENAME}" | sed -ne "s/^SFSC[0-9]\{8,\}_\(.*\)$/\1/p")
 	log 2 "Final filename from ${FILENAME} is ${REALNAME}"	
 	if [ -z "${REALNAME}"  ] ; then
 		# something went wrong
@@ -53,39 +53,39 @@ handle_file() {
 		return	
 	fi
 	# move file to right place
-	mv ${DOWNLOAD_FOLDER}/${FILENAME} ${CASES_FOLDER}/${CASENO}/${REALNAME} ||
+	mv "${DOWNLOAD_FOLDER}/${FILENAME}" "${CASES_FOLDER}/${CASENO}/${REALNAME}" ||
 		log 1 "Could not move file ${DOWNLOAD_FOLDER}/${FILENAME} to ${CASES_FOLDER}/${CASENO}/${REALNAME}"  
 	# check if we need to extract a archive
-	cd ${CASES_FOLDER}/${CASENO} || log 1 "Could not change into case directory ${CASES_FOLDER}/${CASENO}" 
-	TYPE=$(file -b -i ${REALNAME})
+	cd "${CASES_FOLDER}/${CASENO}" || log 1 "Could not change into case directory ${CASES_FOLDER}/${CASENO}" 
+	TYPE=$(file -b -i "${REALNAME}")
 	log 2 "Filetype of ${REALNAME} is ${TYPE}"
 	case "${TYPE}" in
 		*x-bzip2*)
-			if is_tar ${REALNAME} ; then
-				/usr/bin/tar xf ${REALNAME} || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+			if is_tar "${REALNAME}" ; then
+				/usr/bin/tar xf "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			else
-				/usr/bin/bunzip2 ${REALNAME} || log 1 "Could not bunzip2 ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+				/usr/bin/bunzip2 "${REALNAME}" || log 1 "Could not bunzip2 ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			fi 
 			;;
 		*gzip*)
-                        if is_tar ${REALNAME} ; then
-				/usr/bin/tar xf ${REALNAME} || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+                        if is_tar "${REALNAME}" ; then
+				/usr/bin/tar xf "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
 			else
-                                /usr/bin/gunzip ${REALNAME} || log 1 "Could not ungzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+                                /usr/bin/gunzip "${REALNAME}" || log 1 "Could not ungzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
                         fi
                         ;;
                 *x-xz*)
-                        if is_tar ${REALNAME} ; then
-                                /usr/bin/tar xf ${REALNAME} || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+                        if is_tar "${REALNAME}" ; then
+                                /usr/bin/tar xf "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
                         else
-                                /usr/bin/unxz ${REALNAME} || log 1 "Could not unxz ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+                                /usr/bin/unxz "${REALNAME}" || log 1 "Could not unxz ${CASES_FOLDER}/${CASENO}/${REALNAME}"
                         fi
                         ;;
 		*x-tar*)
-			/usr/bin/tar xf ${REALNAME} || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+			/usr/bin/tar xf "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			;;
 		*zip*)
-			/usr/bin/unzip ${REALNAME} || log 1 "Could not unzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+			/usr/bin/unzip "${REALNAME}" || log 1 "Could not unzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			;;
 	esac
 }
@@ -114,11 +114,11 @@ fi
 # MOVED_TO we get if the donwload is moved from the tmp location.
 # If we get ATTRIB we need to wait for a CLOSE_WRITE that indicates download is
 # finished
-inotifywait -m ${DOWNLOAD_FOLDER} -e attrib -e moved_to --exclude '.*\.(crdownload)|(part)$' -q | while read DIR ACTION FILE ; do
+inotifywait -m "${DOWNLOAD_FOLDER}" -e attrib -e moved_to --exclude '.*\.(crdownload)|(part)$' -q | while read DIR ACTION FILE ; do
 	log 2 "Got action ${ACTION} for ${DIR}/${FILE}"
 	# only take care on files with special name
 	# extract the casenumber already for later use
-	CASENO=$(echo ${FILE} | sed -ne "s/^SFSC\([0-9]\{8,\}\)_.*$/\1/p")
+	CASENO=$(echo "${FILE}" | sed -ne "s/^SFSC\([0-9]\{8,\}\)_.*$/\1/p")
 	if [ ! -z "${CASENO}"  ] ; then
 		log 2 "Caseno for ${DIR}/${FILE} is ${CASENO}" 
 		if [ ${ACTION} == "ATTRIB" ] ; then
@@ -132,13 +132,13 @@ inotifywait -m ${DOWNLOAD_FOLDER} -e attrib -e moved_to --exclude '.*\.(crdownlo
 			# additonal use a timeout to no block forever. 5 mins sufficient for max
 			# 35 MB attahcment size.
 			log 2 "Waiting for CLOSE_WRITE action on ${DIR}/${FILE}"
-			( inotifywait ${DIR}/${FILE} -e close_write -e moved_from -q -t 300 | while read DIR ACTION FILE ; do 
+			( inotifywait "${DIR}/${FILE}" -e close_write -e moved_from -q -t 300 | while read DIR ACTION FILE ; do 
 				if [ ${ACTION} != "MOVED_FROM" ] ; then 
-					(handle_file ${FILE} ${CASENO}) & 
+					(handle_file "${FILE}" "${CASENO}") & 
 				fi
 			done ) & 
 		else 
-			( handle_file ${FILE} ${CASENO} ) &	
+			( handle_file "${FILE}" "${CASENO}" ) &	
 		fi
 	fi
 done
