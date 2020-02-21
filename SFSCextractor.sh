c2e23e4d59e989fdbd7a8dee8743e679a7c6a770
Paul Zirnik <paul.zirnik@suse.com>
2020-02-21 17:54:58 +0100

    added unrar - improved inotify handling

--- SFSCextractor.sh
+++ SFSCextractor.sh
@@ -27,7 +27,6 @@ log() {
 # not report an error if the file is compressed
 # with bzip2 but is not a tar archive at all.
 # in this case tar does report success, which is wrong
-# as nothing will be done 
 is_tar() {
 	FILELIST=$(${TAR} --list -f "${1}")
 	if [ -z "${FILELIST}" -o $? -ne 0 ] ; then
@@ -45,16 +44,16 @@ untar() {
 		return -1
 	fi
 	log 1 "tempdir=${tmpdir}"
-	${TAR} xfv "${FILENAME}" | tee ${tmpdir}
+	${TAR} xfv "${FILENAME}" | tee "${tmpdir}" >/dev/null
 	if [ $? -ne 0 ] ; then
 		return -1
 	fi
 	if [ "${FIX_ARCHIVE_RIGHTS}" -eq "1" ] ; then
 		while read dir ; do
-			find ${dir} \( -type d -exec chmod 0755 {} \; -o -type f -exec chmod 0644 {} \; \)
-		done < <(cut -d\/ -f 1 ${tmpdir} | uniq)
+			find "${dir}" \( -type d -exec chmod 0755 {} \; -o -type f -exec chmod 0644 {} \; \)
+		done < <(cut -d\/ -f 1 "${tmpdir}" | uniq)
 	fi
-	rm ${tmpdir}
+	rm "${tmpdir}"
 	return 0;
 }
 
@@ -65,7 +64,7 @@ handle_file() {
 	CASENO=$2
 	log 2 "Need to handle ${DOWNLOAD_FOLDER}/${FILENAME}" 
 	# create directory
-	mkdir -p ${CASES_FOLDER}/${CASENO} || log 1 "Could not create directory ${CASES_FOLDER}/${CASENO}"
+	mkdir -p "${CASES_FOLDER}/${CASENO}" || log 1 "Could not create directory ${CASES_FOLDER}/${CASENO}"
 	# compute real filename
 	REALNAME=$(echo "${FILENAME}" | sed -ne "s/^SFSC[0-9]\{8,\}_\(.*\)$/\1/p")
 	log 2 "Final filename from ${FILENAME} is ${REALNAME}"	
@@ -109,6 +108,9 @@ handle_file() {
 		*zip*)
 			${UNZIP} "${REALNAME}" || log 1 "Could not unzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			;;
+		*x-rar*)
+			${UNRAR} "${REALNAME}" || log 1 "Could not unrar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+			;;
 	esac
 }
 
@@ -117,7 +119,7 @@ handle_file() {
 
 # check if everything is set
 if [ -z "${DOWNLOAD_FOLDER}" ] ; then
-	echo "You need to set your dowloadfolder in ~/.SFSCextratorrc" >&2
+	echo "You need to set your downloadfolder in ~/.SFSCextratorrc" >&2
 	exit 1
 fi
 
@@ -139,9 +141,10 @@ UNZIP=$(which unzip)
 UNXZ=$(which unxz)
 GUNZIP=$(which gunzip)
 BUNZIP2=$(which bunzip2)
+UNRAR=$(which unrar)
 
-if [ -z ${TAR} -o -z ${UNZIP} -o -z ${UNXZ} -o -z ${GUNZIP} -o -z ${BUNZIP2} ] ; then
-	echo "not all extractor tools are installed, check: tar, unzip, unxz, gunzip and bunzip2"
+if [ -z ${TAR} -o -z ${UNZIP} -o -z ${UNXZ} -o -z ${GUNZIP} -o -z ${BUNZIP2} -o -z ${UNRAR} ] ; then
+	echo "not all extractor tools are installed, check: tar, unzip, unxz, gunzip, bunzip2 and unrar"
 	exit 1
 fi
 
@@ -153,9 +156,9 @@ if [ -z ${INOTIFY} ] ; then
 fi
 
 # depending on the tmp folder and the download folder chrome and firefox
-# either donwload the file into a tmp directory and then move the file to
-# the donwload directory or the create the file directly in download directory.
-# MOVED_TO we get if the donwload is moved from the tmp location.
+# either download the file into a tmp directory and then move the file to
+# the download directory. Or they create the file directly in download directory.
+# MOVED_TO we get if the download is moved from the tmp location.
 # If we get ATTRIB we need to wait for a CLOSE_WRITE that indicates download is
 # finished
 ${INOTIFY} -m "${DOWNLOAD_FOLDER}" -e attrib -e moved_to --exclude '.*\.(crdownload)|(part)$' -q | while read DIR ACTION FILE ; do
@@ -167,17 +170,18 @@ ${INOTIFY} -m "${DOWNLOAD_FOLDER}" -e attrib -e moved_to --exclude '.*\.(crdownl
 		log 2 "Caseno for ${DIR}/${FILE} is ${CASENO}" 
 		if [ ${ACTION} == "ATTRIB" ] ; then
 			# firefox does use CLOSE_WRITE prior the download
-			# has been started at all (e.g. touch the file prior donwload). 
-			# So i did wait for ATTRIB that is called shorty after the 
+			# has been started at all (e.g. touch the file prior download). 
+			# So we do wait for ATTRIB that is called shorty after the 
 			# first CLOSE_WRITE, followed by the final CLOSE_WRITE. However 
 			# the later move into the cases folder also triggers a ATTRIB. 
 			# So we need to check if the ATTRIB was caused by handle_file() (MOVED_FROM) 
 			# or is prior the CLOSE_WRITE that indicates download is finished.
 			# additonal use a timeout to no block forever. 5 mins sufficient for max
-			# 35 MB attahcment size.
+			# 35 MB attachment size.
 			log 2 "Waiting for CLOSE_WRITE action on ${DIR}/${FILE}"
-			( ${INOTIFY} "${DIR}/${FILE}" -e close_write -e moved_from -q -t 300 | while read DIR ACTION FILE ; do 
-				if [ ${ACTION} != "MOVED_FROM" ] ; then 
+			( ${INOTIFY} "${DIR}/${FILE}" -e close_write -e moved_from -q -t 300 2>/dev/null | while read DIR ACTION NONE ; do 
+				log 2 "Got action ${ACTION} for ${DIR}/${FILE}"
+				if [ ${ACTION} != "MOVED_FROM" ] ; then
 					(handle_file "${FILE}" "${CASENO}") & 
 				fi
 			done ) & 
