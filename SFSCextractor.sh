f8abd3afe85ad3ef2c3f30f272ec17bbdcc61d81
Paul Zirnik <paul.zirnik@suse.com>
2020-02-25 10:07:50 +0100

    simplified and improved inotify; improved logging

--- SFSCextractor.sh
+++ SFSCextractor.sh
@@ -40,12 +40,12 @@ untar() {
 	FILENAME=$1
 	local tmpdir=$(mktemp)
 	if [ -z "${tmpdir}" ] ; then
-		log 1 "Could not create tempdir for archive extraction"
+		log 1 "${FUNCNAME[0]}: Could not create tempdir for archive extraction"
 		return -1
 	fi
-	log 1 "tempdir=${tmpdir}"
 	${TAR} xfv "${FILENAME}" | tee "${tmpdir}" >/dev/null
 	if [ $? -ne 0 ] ; then
+		rm -f "${tmpdir}"
 		return -1
 	fi
 	if [ "${FIX_ARCHIVE_RIGHTS}" -eq "1" ] ; then
@@ -53,7 +53,7 @@ untar() {
 			find "${dir}" \( -type d -exec chmod 0755 {} \; -o -type f -exec chmod 0644 {} \; \)
 		done < <(cut -d\/ -f 1 "${tmpdir}" | uniq)
 	fi
-	rm "${tmpdir}"
+	rm -f "${tmpdir}"
 	return 0;
 }
 
@@ -62,54 +62,54 @@ untar() {
 handle_file() {
 	FILENAME=$1
 	CASENO=$2
-	log 2 "Need to handle ${DOWNLOAD_FOLDER}/${FILENAME}" 
+	log 2 "${FUNCNAME[0]}: Need to handle ${DOWNLOAD_FOLDER}/${FILENAME}" 
 	# create directory
-	mkdir -p "${CASES_FOLDER}/${CASENO}" || log 1 "Could not create directory ${CASES_FOLDER}/${CASENO}"
+	mkdir -p "${CASES_FOLDER}/${CASENO}" || log 1 "${FUNCNAME[0]}: Could not create directory ${CASES_FOLDER}/${CASENO}"
 	# compute real filename
 	REALNAME=$(echo "${FILENAME}" | sed -ne "s/^SFSC[0-9]\{8,\}_\(.*\)$/\1/p")
-	log 2 "Final filename from ${FILENAME} is ${REALNAME}"	
+	log 2 "${FUNCNAME[0]}: Final filename from ${FILENAME} is ${REALNAME}"	
 	if [ -z "${REALNAME}"  ] ; then
 		# something went wrong
-		log 1 "Could not get real filename from: ${FILENAME}"
+		log 1 "${FUNCNAME[0]}: Could not get real filename from: ${FILENAME}"
 		return	
 	fi
 	# move file to right place
 	mv "${DOWNLOAD_FOLDER}/${FILENAME}" "${CASES_FOLDER}/${CASENO}/${REALNAME}" ||
-		log 1 "Could not move file ${DOWNLOAD_FOLDER}/${FILENAME} to ${CASES_FOLDER}/${CASENO}/${REALNAME}"  
+		log 1 "${FUNCNAME[0]}: Could not move file ${DOWNLOAD_FOLDER}/${FILENAME} to ${CASES_FOLDER}/${CASENO}/${REALNAME}"  
 	# check if we need to extract a archive
-	cd "${CASES_FOLDER}/${CASENO}" || log 1 "Could not change into case directory ${CASES_FOLDER}/${CASENO}" 
+	cd "${CASES_FOLDER}/${CASENO}" || log 1 "${FUNCNAME[0]}: Could not change into case directory ${CASES_FOLDER}/${CASENO}" 
 	TYPE=$(file -b -i "${REALNAME}")
-	log 2 "Filetype of ${REALNAME} is ${TYPE}"
+	log 2 "${FUNCNAME[0]}: Filetype of ${REALNAME} is ${TYPE}"
 	case "${TYPE}" in
 		*x-bzip2*)
 			if is_tar "${REALNAME}" ; then
-				untar "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+				untar "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			else
-				${BUNZIP2} "${REALNAME}" || log 1 "Could not bunzip2 ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+				${BUNZIP2} "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not bunzip2 ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			fi 
 			;;
 		*gzip*)
                         if is_tar "${REALNAME}" ; then
-				untar "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+				untar "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
 			else
-                                ${GUNZIP} "${REALNAME}" || log 1 "Could not ungzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+                                ${GUNZIP} "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not ungzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
                         fi
                         ;;
                 *x-xz*)
                         if is_tar "${REALNAME}" ; then
-                                untar "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+                                untar "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
                         else
-                                ${UNXZ} "${REALNAME}" || log 1 "Could not unxz ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+                                ${UNXZ} "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not unxz ${CASES_FOLDER}/${CASENO}/${REALNAME}"
                         fi
                         ;;
 		*x-tar*)
-			untar "${REALNAME}" || log 1 "Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+			untar "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not untar ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			;;
 		*zip*)
-			${UNZIP} "${REALNAME}" || log 1 "Could not unzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
+			${UNZIP} "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not unzip ${CASES_FOLDER}/${CASENO}/${REALNAME}" 
 			;;
 		*x-rar*)
-			${UNRAR} "${REALNAME}" || log 1 "Could not unrar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
+			${UNRAR} "${REALNAME}" || log 1 "${FUNCNAME[0]}: Could not unrar ${CASES_FOLDER}/${CASENO}/${REALNAME}"
 			;;
 	esac
 }
@@ -161,32 +161,32 @@ fi
 # MOVED_TO we get if the download is moved from the tmp location.
 # If we get ATTRIB we need to wait for a CLOSE_WRITE that indicates download is
 # finished
-${INOTIFY} -m "${DOWNLOAD_FOLDER}" -e attrib -e moved_to --exclude '.*\.(crdownload)|(part)$' -q | while read DIR ACTION FILE ; do
-	log 2 "Got action ${ACTION} for ${DIR}/${FILE}"
+ATTRIB_SEEN=0
+${INOTIFY} -m "${DOWNLOAD_FOLDER}" -e attrib -e moved_to -e close_write -e moved_from --exclude '.*\.(crdownload)|(part)$' -q | while read DIR ACTION FILE ; do
+	log 2 "${FUNCNAME[0]}: ${ACTION} action for ${DIR}/${FILE}"
 	# only take care on files with special name
 	# extract the casenumber already for later use
 	CASENO=$(echo "${FILE}" | sed -ne "s/^SFSC\([0-9]\{8,\}\)_.*$/\1/p")
 	if [ ! -z "${CASENO}"  ] ; then
-		log 2 "Caseno for ${DIR}/${FILE} is ${CASENO}" 
-		if [ ${ACTION} == "ATTRIB" ] ; then
-			# firefox does use CLOSE_WRITE prior the download
-			# has been started at all (e.g. touch the file prior download). 
-			# So we do wait for ATTRIB that is called shorty after the 
-			# first CLOSE_WRITE, followed by the final CLOSE_WRITE. However 
-			# the later move into the cases folder also triggers a ATTRIB. 
-			# So we need to check if the ATTRIB was caused by handle_file() (MOVED_FROM) 
-			# or is prior the CLOSE_WRITE that indicates download is finished.
-			# additonal use a timeout to no block forever. 5 mins sufficient for max
-			# 35 MB attachment size.
-			log 2 "Waiting for CLOSE_WRITE action on ${DIR}/${FILE}"
-			( ${INOTIFY} "${DIR}/${FILE}" -e close_write -e moved_from -q -t 300 2>/dev/null | while read DIR ACTION NONE ; do 
-				log 2 "Got action ${ACTION} for ${DIR}/${FILE}"
-				if [ ${ACTION} != "MOVED_FROM" ] ; then
-					(handle_file "${FILE}" "${CASENO}") & 
+		case ${ACTION} in
+			"CLOSED_WRITE")
+				if [ "${ATTRIB_SEEN}" -eq 1 ] ; then
+					log 2 "${FUNCNAME[0]}: Caseno for ${DIR}/${FILE} is ${CASENO}"
+					ATTRIB_SEEN=0
+					(handle_file "${FILE}" "${CASENO}") &
 				fi
-			done ) & 
-		else 
-			( handle_file "${FILE}" "${CASENO}" ) &	
-		fi
+				;;
+			"ATTRIB")
+				ATTRIB_SEEN=1
+				;;
+			"MOVED_TO")
+				log 2 "${FUNCNAME[0]}: Caseno for ${DIR}/${FILE} is ${CASENO}"
+				ATTRIB_SEEN=0
+				(handle_file "${FILE}" "${CASENO}") &
+				;;
+			*)
+				ATTRIB_SEEN=0
+				;;
+		esac 
 	fi
 done
